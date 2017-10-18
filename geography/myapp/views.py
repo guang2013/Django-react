79a67cc64023fdc49872302e7bdd91168551275a
Guangwen Lu <guangwen.lu@waypay.ca>
2017-10-17 21:57:24 -0400

    Django-backend

---
+++ geography/myapp/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render
+
+# Create your views here.
+# coding:utf-8
+import json
+from django.http import HttpResponse
+from myapp.models import *
+from django.views.decorators.csrf import csrf_exempt
+from django.db.models.query import QuerySet
+import datetime
+
+
+def product_list(request):
+    date_from = request.GET.get('start_time','2016-10-12T12:00:00-05:00')
+    date_to = request.GET.get('end_time', '2016-10-14T12:00:00-05:00')
+    data = []
+    for item in  MyData.objects.filter(datetime__range=(date_from, date_to)):
+        data.append({
+            'id':item.id,
+            'sid':item.sid,
+            'description':item.description,
+            'datetime': str(item.datetime),
+            'longitude':item.longitude,
+            'latitude':item.latitude,
+            'elevation':item.elevation,
+        })
+    res = json.dumps(data)
+
+    return HttpResponse(res)
+
+@csrf_exempt
+def product_update(request):
+
+    data = json.loads(request.body)
+    print(data)
+    id = data.get('id')
+    sid = data.get('sid')
+    description = data.get('description')
+    datetime = data.get('datetime')
+    longitude = data.get('longitude')
+    latitude = data.get('latitude')
+    elevation = data.get('elevation')
+    print(id)
+    myData = MyData.objects.get(id=id)
+    myData.sid =sid
+    myData.description = description
+    myData.datetime = datetime
+    myData.longitude = longitude
+    myData.latitude = latitude
+    myData.elevation = elevation
+
+    myData.save()
+    return HttpResponse(u"200")
+
+@csrf_exempt
+def product_add(request):
+    myData=MyData(
+        # id=request.POST.get('id'),
+        sid = request.POST.get('sid'),
+        description = request.POST.get('description'),
+        datetime = request.POST.get('datetime'),
+        longitude = request.POST.get('longitude'),
+        latitude = request.POST.get('latitude'),
+        elevation = request.POST.get('elevation'),
+    )
+    myData.save()
+    return HttpResponse(u"200")
+
+def product_list_1(request):
+    date_from = request.POST.get('start_time','2016-10-12T12:00:00-05:00')
+    date_to = request.POST.get('end_time', '2016-10-14T12:00:00-05:00')
+    products = request.POST.get('products', ['Cesna 120'])
+
+    data = []
+
+    a = MyData.objects.filter(description__in=products,datetime__range=(date_from, date_to))
+    for item in a:
+        print(item)
+        data.append({
+            'id':item.id,
+            'sid':item.sid,
+            'description':item.description,
+            'datetime': str(item.datetime),
+            'longitude':item.longitude,
+            'latitude':item.latitude,
+            'elevation':item.elevation,
+        })
+    res = json.dumps(data)
+    return HttpResponse(res)
+
+
+def product_delete(request):
+    id = request.GET.get('id')
+    MyData.objects.filter(id=id).delete()
+    return HttpResponse('200')
---
+++ myapp/utils.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+import codecs
+import os
+import django
+import docx
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','geography.settings')
+django.setup()
+from myapp.models import MyData
+
+
+def import_data():
+    doc = docx.Document('./input.txt.docx')
+    paras = doc.paragraphs
+    data = []
+    for index,p in enumerate(paras):
+        if index!=0 :
+            item = p.text.split('\t')
+            data.append(MyData(
+                sid=item[0],
+                description=item[1],
+                datetime=item[2],
+                longitude=item[3],
+                latitude=item[4],
+                elevation=item[5],
+            ) )
+    MyData.objects.bulk_create(data)
+    print('导入数据成功')
+    # print('\n'.join(data))
+
+def export_data():
+    document = docx.Document()
+
+
+    items = MyData.objects.all()
+    for item in items:
+        pass # 此处导出
+
+
+if __name__ == '__main__':
+    import_data()
+    export_data()
